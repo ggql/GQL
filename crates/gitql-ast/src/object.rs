0f7b63b6178489ac4d78c415b71f2cc571e520ed
Jia Jia <angersax@sina.com>
2023-12-17 10:54:53 +0800

    feat(gitql-ast): add support for testing

--- crates/gitql-ast/src/object.rs
+++ crates/gitql-ast/src/object.rs
@@ -16,3 +16,35 @@ pub fn flat_gql_groups(groups: &mut Vec<Vec<GQLObject>>) {
     groups.clear();
     groups.push(main_group);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_flat_gql_groups() {
+        let mut group: Vec<GQLObject> = Vec::new();
+        let mut groups: Vec<Vec<GQLObject>> = Vec::new();
+
+        flat_gql_groups(&mut groups);
+        assert_eq!(groups.len(), 1);
+
+        for item in groups.iter() {
+            assert_eq!(item.len(), 0);
+        }
+
+        group.push(GQLObject{ attributes: Default::default()});
+
+        groups.clear();
+        groups.push(group.to_owned());
+        groups.push(group.to_owned());
+        assert_eq!(groups.len(), 2);
+
+        flat_gql_groups(&mut groups);
+        assert_eq!(groups.len(), 1);
+
+        for item in groups.iter() {
+            assert_eq!(item.len(), 2);
+        }
+    }
+}
