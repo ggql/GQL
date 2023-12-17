2f12f6c6a218b5a7d9c4f4bc2ee1c55d25d67355
Jia Jia <angersax@sina.com>
2023-12-17 09:46:30 +0800

    feat(scripts): update script for testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,10 @@
 
 build=$(date +%FT%T%z)
 
-# rustup update --no-self-update stable
-build=$build cargo test --all-features --all-targets -- --nocapture
+list="gitql-ast gitql-cli gitql-engine gitql-parser"
+for item in $list; do
+  pushd crates/"$item" || exit
+  # rustup update --no-self-update stable
+  build=$build cargo test --all-features --all-targets -- --nocapture
+  popd || exit
+done
