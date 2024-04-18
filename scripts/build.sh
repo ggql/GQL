e2fb9609f630fef74a25c0304483806938a49a66
Jia <49358172+craftslab@users.noreply.github.com>
2024-04-18 10:39:45 +0000

    feat(build): refactor code for building

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-
 build=$(date +%FT%T%z)
-
 linux="target/x86_64-unknown-linux-musl/release/gitql"
-windows="target/x86_64-pc-windows-gnu/release/gitql.exe"
-
 if [ "$1" = "all" ]; then
-  build=$build cargo build --release --all-features --all-targets --target=x86_64-pc-windows-gnu
-  build=$build cargo build --release --all-features --all-targets --target=x86_64-unknown-linux-musl
+  build=$build cargo build --release --all-features --all-targets
 elif [ "$1" = "offline" ]; then
-  build=$build cargo build --release --all-features --all-targets --target=x86_64-pc-windows-gnu --offline
-  build=$build cargo build --release --all-features --all-targets --target=x86_64-unknown-linux-musl --offline
+  build=$build cargo build --release --all-features --all-targets --offline
 elif [ "$1" = "check" ]; then
   build=$build cargo check --release --all-features --all-targets
 else
-  build=$build cargo build --release --all-features --all-targets --target=x86_64-unknown-linux-musl
+  build=$build cargo build --release --all-features --all-targets
 fi
-
 if [ -f $linux ]; then upx $linux; fi
-if [ -f $windows ]; then upx $windows; fi
