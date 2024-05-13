02ae1a80cad55a31486744ba308421385b0b0fa4
Jia <49358172+craftslab@users.noreply.github.com>
2024-05-13 09:42:02 +0800

    refactor(scripts): refactor code

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 build=$(date +%FT%T%z)
+
 linux="target/release/gitql"
+
 if [ "$1" = "all" ]; then
   build=$build cargo build --release --all-features --all-targets
 elif [ "$1" = "offline" ]; then
@@ -10,4 +13,5 @@ elif [ "$1" = "check" ]; then
 else
   build=$build cargo build --release --all-features --all-targets
 fi
+
 if [ -f $linux ]; then upx $linux; fi
